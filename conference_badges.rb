c1df849ddecc6c94ebe8190da6908a2b69d9ce70
Brad Hambrice <hambrice@uab.edu>
2018-01-04 23:13:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,21 +1,25 @@
-def badge_maker (name)
+def badge_maker(name)
   return "Hello, my name is #{name}."
 end
-def batch_badge_creator (array)
+def batch_badge_creator(array)
   new_array=[]
   array.each do |name|
     new_array.push("Hello, my name is #{name}.")
   end
     new_array
   end
-  def assign_rooms (array)
+  def assign_rooms(array)
     new_array = []
     array.each do |name|
       new_array.push("Hello, #{name}! You'll be assigned to room #{array.index(name) + 1}!")
     end
     new_array
   end
-  def printer (array)
-   puts batch_badge_creator(array)
-   puts assign_rooms(array)
+  def printer(array)
+   batch_badge_creater(array).each do |response|
+     puts response
+   end
+   assign_rooms(array).each do |response|
+     puts response
+   end
  end
